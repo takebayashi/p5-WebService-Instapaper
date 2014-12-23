c52c67fc2a622219cf247d835819e6fb29446dc5
Shun Takebayashi <shun@takebayashi.asia>
2014-12-23 13:57:12 +0900

    Create repo

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Instapaper - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Instapaper;
+
+# DESCRIPTION
+
+WebService::Instapaper is ...
+
+# LICENSE
+
+Copyright (C) Shun Takebayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Shun Takebayashi <shun@takebayashi.asia>
---
+++ lib/WebService/Instapaper.pm
@@ -0,0 +1,39 @@
+package WebService::Instapaper;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Instapaper - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Instapaper;
+
+=head1 DESCRIPTION
+
+WebService::Instapaper is ...
+
+=head1 LICENSE
+
+Copyright (C) Shun Takebayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Shun Takebayashi E<lt>shun@takebayashi.asiaE<gt>
+
+=cut
+
